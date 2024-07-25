6f5f41b243be9a3b0f2f7c9d458d57001c38f630
Saurabh Singh <singhsaurabh1606@gmail.com>
2024-07-25 11:41:54 +0530

    first commit

---
+++ js/Typed.js
@@ -0,0 +1,11 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed("#typed-text", {
+      strings: ["Hello I'm Saurabh Singh"],
+      typeSpeed: 50, // Speed in milliseconds for each character
+      backSpeed: 25, // Speed in milliseconds for backspacing
+      loop: false,   // Whether to loop the text
+      showCursor: true, // Show cursor
+    });
+  });
+
